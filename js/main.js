ce72658cbddddb534f84aadb9341e111b4071530
Omar Saidi <linmff80@gmail.com>
2019-02-13 17:38:57 +0100

    sass included

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ var signin = document.getElementById('signin');
 var signup = document.getElementById('signup');
 
 document.getElementById('sign-in').addEventListener('click', function () {
-  signin.classList.add('show-signin');
+  signin.classList.add('show-sign');
 });
 document.getElementById('close-signin').addEventListener('click', function () {
-  signin.classList.remove('show-signin');
+  signin.classList.remove('show-sign');
 });
 
 document.getElementById('sign-up').addEventListener('click', function () {
   signin.classList.remove('show-signin');
-  signup.classList.add('show-signup');
+  signup.classList.add('show-sign');
 
 });
 document.getElementById('close-signup').addEventListener('click', function () {
-  signup.classList.remove('show-signup');
+  signup.classList.remove('show-sign');
 });
